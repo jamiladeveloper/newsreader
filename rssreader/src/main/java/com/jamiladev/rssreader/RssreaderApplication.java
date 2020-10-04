d7e03846f3037f3845e742eda75fba73ce7e4f9b
akg <akg.bit@gmail.com>
2020-10-04 09:07:52 +0530

    RSS Feed code updated

--- rssreader/src/main/java/com/jamiladev/rssreader/RssreaderApplication.java
+++ rssreader/src/main/java/com/jamiladev/rssreader/RssreaderApplication.java
@@ -3,11 +3,21 @@ package com.jamiladev.rssreader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class RssreaderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RssreaderApplication.class, args);
+
+		System.setProperty("http.agent", "Chrome");
+
+		Feed feed = new RssReader("http://www.thehindu.com/news/feeder/default.rss").readFeed();
+		List<FeedMessage> feeds = feed.entries;
+		for(FeedMessage message : feeds) {
+			System.out.println(message);
+		}
 	}
 
 }
