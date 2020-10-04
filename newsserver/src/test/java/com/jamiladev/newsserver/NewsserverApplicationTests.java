fbf59c913d0f84dcf77fe10d3e710845861bcd39
akg <akg.bit@gmail.com>
2020-10-04 08:39:10 +0530

    First commit

---
+++ newsserver/src/test/java/com/jamiladev/newsserver/NewsserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamiladev.newsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ rssreader/src/main/java/com/jamiladev/rssreader/RssreaderApplication.java
@@ -0,0 +1,13 @@
+package com.jamiladev.rssreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssreaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssreaderApplication.class, args);
+	}
+
+}
